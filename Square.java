43b3560c4e24e164e75ede8ddf9ef68a7a083c06
htetyadanar-hyo <149476965+htetyadanar-hyo@users.noreply.github.com>
2024-03-06 15:13:26 -0600

    Update Square.java

--- Square.java
+++ Square.java
@@ -1,7 +1,7 @@
 package Assignment5;
 
 class Square extends Rectangle{
-    public Square(String color,double width,double height){
-        super(color, width, height);
+    public Square(String color,double side){
+        super(color, side, side);
     }
 }
