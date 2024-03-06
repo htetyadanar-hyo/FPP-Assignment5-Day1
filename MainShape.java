631e6b609903a84164af4d5a9f5cfe0cfaf00374
htetyadanar-hyo <149476965+htetyadanar-hyo@users.noreply.github.com>
2024-03-06 15:14:06 -0600

    Update MainShape.java

--- MainShape.java
+++ MainShape.java
@@ -5,8 +5,8 @@ public class MainShape {
         Shape[] type = new Shape[5];
         type[0] = new Rectangle("black",2,3);
         type[1] = new Circle("white",2);
-        type[2] = new Square("pink",2,2);
-        type[3] = new Square("grey",3,3);
+        type[2] = new Square("pink",2);
+        type[3] = new Square("grey",3);
         type[4] = new Circle("pink",3.02);
         printTotal(type);
     }
